7981b6dd555fefa7ff6172f867ba4ed6f7971c6c
Alec <Alec>
2016-10-28 15:15:44 -0500

    initial

---
+++ src/neuralnetwork/Perceptron.java
@@ -0,0 +1,39 @@
+package neuralnetwork;
+
+public class Perceptron {
+	
+	public static final int[][][] andData = {{{0,0},{1}},
+											 {{0,1},{1}},
+											 {{1,0},{1}},
+											 {{1,1},{0}}};
+	/*public static final int[][][] andData = {{{0,0},{0}},
+											 {{0,1},{1}},
+											 {{1,0},{1}},
+											 {{1,1},{0}}};
+	public static final int[][][] andData = {{{0,0},{0}},
+											 {{0,1},{1}},
+											 {{1,0},{1}},
+											 {{1,1},{1}}};
+	*/
+	public static final int[][][] testData = {{{1,1}},
+											 {{0,1}},
+											 {{1,0}},
+											 {{1,1}}};
+	public static final double LEARNING_RATE = 0.005;
+	public static final double[] INITIAL_WEIGHTS = {Math.random(),Math.random()};
+	public double calculateWeightedSum(int[] data,double[] weights){
+		double weightedSum = 0;
+		for(int x=0; x< data.length; x++) weightedSum += data[x] * weights[x];
+		return weightedSum;
+	}
+	public int applyActivationFunction(double weightedSum){
+		int result = 0;
+		if(weightedSum > 1) result = 1;
+		return result;
+	}
+	public double[] adjustWeights(int[] data, double[] weights, double error){
+		double[] adjustedWeights = new double[weights.length];
+		for(int x=0; x < weights.length; x++) adjustedWeights[x] = LEARNING_RATE * error + weights[x];// * data[x]
+		return adjustedWeights;
+	}
+}
